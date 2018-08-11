3a6bf59431c0d3469d067182b32db459c07367d0
Brooks McKinley <brooksmckinley@yahoo.com>
2018-08-10 22:37:05 -0400

    removed a large amount of repeated code using macros

--- src/cup.rs
+++ src/cup.rs
@@ -1,12 +1,38 @@
 use rand::Rng;
 
 pub trait Cup {
-    fn fill(&mut self, amount: f64);
-    fn empty(&mut self, amount: f64);
-    fn drop_cup(&mut self);
-    fn break_cup(&mut self);
+    fn fill(&mut self, amount: f64) {
+        if self.current_fluid() + amount >= self.max_fluid() {
+            let max_fluid = self.max_fluid();
+            self.set_current_fluid(max_fluid);
+        }
+        else {
+            let current_fluid = self.current_fluid();
+            self.set_current_fluid(current_fluid + amount);
+        }
+    }
+    fn empty(&mut self, amount: f64) {
+        if self.current_fluid() - amount <= 0.0 {
+            self.set_current_fluid(0.0);
+        }
+        else {
+            let current_fluid = self.current_fluid();
+            self.set_current_fluid(current_fluid - amount);
+        }
+    }
+    fn drop_cup(&mut self) {
+        let mut rng = ::rand::thread_rng();
+        let chance = rng.gen_range(1, 10);
+        if chance < 3 {
+            self.set_is_broken(true);
+        }
+    }
+    fn break_cup(&mut self) {
+        self.set_is_broken(true);
+    }
+
     
-    // Fields
+    // Getters
     fn name(&self) -> String;
     fn serial(&self) -> i32;
     fn color(&self) -> String;
@@ -14,229 +40,167 @@ pub trait Cup {
     fn max_fluid(&self) -> f64;
     fn current_fluid(&self) -> f64;
     fn is_broken(&self) -> bool;
+
+    // Setters
+    fn set_name(&mut self, name: String);
+    fn set_serial(&mut self, serial: i32);
+    fn set_color(&mut self, color: String);
+    fn set_max_fluid(&mut self, max_fluid: f64);
+    fn set_current_fluid(&mut self, current_fluid: f64);
+    fn set_is_broken(&mut self, is_broken: bool);
 }
 
-pub struct Mug {
-    name: String,
-    serial_number: i32,
-    color: String,
-    handle: bool,
-    max_fluid: f64,
-    current_fluid: f64,
-    is_broken: bool,
+// Setter/Getter macro
+macro_rules! cup_sg {
+    () => {
+        fn name(&self) -> String {
+            self.value_name.clone()
+        }
+        fn serial(&self) -> i32 {
+            self.value_serial.clone()
+        }
+        fn color(&self) -> String {
+            self.value_color.clone()
+        }
+        fn handle(&self) -> bool {
+            self.value_handle.clone()
+        }
+        fn max_fluid(&self) -> f64 {
+            self.value_max_fluid.clone()
+        }
+        fn current_fluid(&self) -> f64 {
+            self.value_current_fluid.clone()
+        }
+        fn is_broken(&self) -> bool {
+            self.value_is_broken.clone()
+        }
+        fn set_name(&mut self, name: String) {
+            self.value_name = name;
+        }
+        fn set_serial(&mut self, serial: i32) {
+            self.value_serial = serial;
+        }
+        fn set_color(&mut self, color: String) {
+            self.value_color = color;
+        }
+        fn set_max_fluid(&mut self, max_fluid: f64) {
+            self.value_max_fluid = max_fluid;
+        }
+        fn set_current_fluid(&mut self, current_fluid: f64) {
+            self.value_current_fluid = current_fluid;
+        }
+        fn set_is_broken(&mut self, is_broken: bool) {
+            self.value_is_broken = is_broken;
+        }
+    };
 }
+
+macro_rules! cup {
+    ( $x:ident ) => {
+        pub struct $x {
+            value_name: String,
+            value_serial: i32,
+            value_color: String,
+            value_handle: bool,
+            value_max_fluid: f64,
+            value_current_fluid: f64,
+            value_is_broken: bool,
+        }
+    };
+}
+
+cup!(Mug);
 
 impl Mug {
     pub fn new(name: String, serial_number: i32, color: String, handle: bool, max_fluid: f64) -> Mug {
         Mug {
-            name: name,
-            serial_number: serial_number,
-            color: color,
-            handle: handle,
-            max_fluid: max_fluid,
-            current_fluid: 0.0,
-            is_broken: false,
+            value_name: name,
+            value_serial: serial_number,
+            value_color: color,
+            value_handle: handle,
+            value_max_fluid: max_fluid,
+            value_current_fluid: 0.0,
+            value_is_broken: false,
         }
     }
 }
 
 impl Cup for Mug {
-    fn fill(&mut self, amount: f64) {
-        if self.current_fluid + amount >= self.max_fluid {
-            self.current_fluid = self.max_fluid;
-        }
-        else {
-            self.current_fluid += amount;
-        }
-    }
-    fn empty(&mut self, amount: f64) {
-        if self.current_fluid - amount <= 0.0 {
-            self.current_fluid = 0.0;
-        }
-        else {
-            self.current_fluid -= amount;
-        }
-    }
-    fn drop_cup(&mut self) {
-        let mut rng = ::rand::thread_rng();
-        let chance = rng.gen_range(1, 10);
-        if chance < 3 {
-            self.is_broken = true;
-        }
-    }
-    fn break_cup(&mut self) {
-        self.is_broken = true;
-    }
-
-    fn name(&self) -> String {
-        self.name.clone()
-    }
-    fn serial(&self) -> i32 {
-        self.serial_number.clone()
-    }
-    fn color(&self) -> String {
-        self.color.clone()
-    }
-    fn handle(&self) -> bool {
-        self.handle.clone()
-    }
-    fn max_fluid(&self) -> f64 {
-        self.max_fluid.clone()
-    }
-    fn current_fluid(&self) -> f64 {
-        self.current_fluid.clone()
-    }
-    fn is_broken(&self) -> bool {
-        self.is_broken
-    }
+    cup_sg!();
 }
 
-pub struct Glass {
-    name: String,
-    serial_number: i32,
-    color: String,
-    handle: bool,
-    max_fluid: f64,
-    current_fluid: f64,
-    is_broken: bool,
-}
+cup!(Glass);
 
 impl Glass {
     pub fn new(name: String, serial_number: i32, color: String, handle: bool, max_fluid: f64) -> Mug {
         Mug {
-            name: name,
-            serial_number: serial_number,
-            color: color,
-            handle: handle,
-            max_fluid: max_fluid,
-            current_fluid: 0.0,
-            is_broken: false,
+            value_name: name,
+            value_serial: serial_number,
+            value_color: color,
+            value_handle: handle,
+            value_max_fluid: max_fluid,
+            value_current_fluid: 0.0,
+            value_is_broken: false,
         }
     }
 }
 
 impl Cup for Glass {
-    fn fill(&mut self, amount: f64) {
-        if self.current_fluid + amount >= self.max_fluid {
-            self.current_fluid = self.max_fluid;
-        }
-        else {
-            self.current_fluid += amount;
-        }
-    }
-    fn empty(&mut self, amount: f64) {
-        if self.current_fluid - amount <= 0.0 {
-            self.current_fluid = 0.0;
-        }
-        else {
-            self.current_fluid -= amount;
-        }
-    }
     fn drop_cup(&mut self) {
         let mut rng = ::rand::thread_rng();
         let chance = rng.gen_range(1, 10);
+        // Glasses have an 80% chance of breaking when dropped
         if chance < 9 {
-            self.is_broken = true;
+            self.set_is_broken(true);
         }
     }
-    fn break_cup(&mut self) {
-        self.is_broken = true;
-    }
-    
-    fn name(&self) -> String {
-        self.name.clone()
-    }
-    fn serial(&self) -> i32 {
-        self.serial_number.clone()
-    }
-    fn color(&self) -> String {
-        self.color.clone()
-    }
-    fn handle(&self) -> bool {
-        self.handle.clone()
-    }
-    fn max_fluid(&self) -> f64 {
-        self.max_fluid.clone()
-    }
-    fn current_fluid(&self) -> f64 {
-        self.current_fluid.clone()
-    }
-    fn is_broken(&self) -> bool {
-        self.is_broken
-    }
+    cup_sg!();
 }
 
-
-pub struct Tumbler {
-    name: String,
-    serial_number: i32,
-    color: String,
-    handle: bool,
-    max_fluid: f64,
-    current_fluid: f64,
-    is_broken: bool,
-}
+cup!(Tumbler);
 
 impl Tumbler {
     pub fn new(name: String, serial_number: i32, color: String, max_fluid: f64) -> Mug {
         Mug {
-            name: name,
-            serial_number: serial_number,
-            color: color,
-            handle: false,
-            max_fluid: max_fluid,
-            current_fluid: 0.0,
-            is_broken: false,
+            value_name: name,
+            value_serial: serial_number,
+            value_color: color,
+            value_handle: false,
+            value_max_fluid: max_fluid,
+            value_current_fluid: 0.0,
+            value_is_broken: false,
         }
     }
 }
 
 impl Cup for Tumbler {
     fn fill(&mut self, amount: f64) {
-        if self.current_fluid + amount >= self.max_fluid {
-            self.current_fluid = self.max_fluid;
+        if self.current_fluid() + amount >= self.max_fluid() {
+            let max_fluid = self.max_fluid();
+            self.set_current_fluid(max_fluid);
         }
         else {
-            self.current_fluid += amount;
+            let current_fluid = self.current_fluid();
+            self.set_current_fluid(current_fluid + amount);
         }
-        self.current_fluid -= self.current_fluid * 0.2;
+        // Tumblers leak
+        let current_fluid = self.current_fluid();
+        self.set_current_fluid(current_fluid - (current_fluid * 0.2));
     }
     fn empty(&mut self, amount: f64) {
-        if self.current_fluid - amount <= 0.0 {
-            self.current_fluid = 0.0;
+        if self.current_fluid() - amount <= 0.0 {
+            self.set_current_fluid(0.0);
         }
         else {
-            self.current_fluid -= amount;
+            let current_fluid = self.current_fluid();
+            self.set_current_fluid(current_fluid - amount);
         }
+        // Tumblers leak
+        let current_fluid = self.current_fluid();
+        self.set_current_fluid(current_fluid - (current_fluid * 0.2));
     }
     fn drop_cup(&mut self) {
         // Tumblers don't break when dropped
     }
-    fn break_cup(&mut self) {
-        self.is_broken = true;
-    }
-    
-    fn name(&self) -> String {
-        self.name.clone()
-    }
-    fn serial(&self) -> i32 {
-        self.serial_number.clone()
-    }
-    fn color(&self) -> String {
-        self.color.clone()
-    }
-    fn handle(&self) -> bool {
-        self.handle.clone()
-    }
-    fn max_fluid(&self) -> f64 {
-        self.max_fluid.clone()
-    }
-    fn current_fluid(&self) -> f64 {
-        self.current_fluid.clone()
-    }
-    fn is_broken(&self) -> bool {
-        self.is_broken.clone()
-    }
+    cup_sg!();
 }
-
-
